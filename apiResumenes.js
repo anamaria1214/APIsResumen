208e6b616ed4488e472aa063c74cb5b61948346f
Ana María <anicu2314@gmail.com>
2025-01-25 16:10:30 -0500

    Versión final con llamado a 2 APIs

--- apiResumenes.js
+++ apiResumenes.js
@@ -1,57 +1,65 @@
-const resumir = async (texto) => {
-    const apis = [
-      fetch(
-        "https://api.meaningcloud.com/summarization-1.0",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams({
-            key: "cff1883b7ba445bf31dc533e28e2ff18",
-            txt: texto, 
-            sentences: 1, 
-            lang: "es", 
-          }),
-        }
-      ),
-      
-    ];
-  
-    try {
-      const respuesta = await Promise.race(apis);
-      const data = await respuesta.json();
-  
-      console.log("Respuesta de la API:", data);
-
-      let textoResumido = "";
-  
-      if (data.summary) {
-        textoResumido = data.summary; 
-      } else if (data.choices) {
-        textoResumido = data.choices[0].message.content; 
-      } else {
-        textoResumido = "No se pudo resumir";
-      }
-  
-      return textoResumido; 
-    } catch (error) {
-      console.error("Error al procesar la API:", error);
-      throw new Error("Error al resumir el texto");
+async function resumirIA1(texto) {
+    const body= {
+            contents: [{
+              parts: [{ text: `Resume el siguiente texto de manera breve solo si es algo valido y con sentido, sino indica que no puedes hablar del tema y pide que se te brinde de nuevo algo valido: ${texto}` }],       
+             }]
+          
     }
+
+    const respuesta= await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDRO7QuYXn26BHrcsOASIxF4i88-WdGk8I", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+    })
+
+    if(!respuesta.ok){
+        throw new Error("Error en la API de gemini");
+    }
+
+    const data= await respuesta.json();
+    return `Resumen generado por Gemini: "${data.candidates[0].content.parts[0].text}"`
+
+}
+
+async function resumirIA2(texto) {
+    const body= {
+        key: "cff1883b7ba445bf31dc533e28e2ff18",
+        txt: texto,
+        sentences: 1,
+        lang: "es",
+          
+    }
+
+    const respuesta= await fetch("https://api.meaningcloud.com/summarization-1.0", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(body)
+    })
+
+    if(!respuesta.ok){
+        throw new Error("Error en la API de MeaningCloud");
+    }
+
+    const data= await respuesta.json();
+    return `Resumen generado por MeaningCloud: "${data.summary}"`
+
+}
+
+const resumir = async () => {
+    const textoOriginal = document.getElementById("texto").value;
+    try {
+        const serviciosIA = [resumirIA1(textoOriginal), resumirIA2(textoOriginal)];
+        const resultado = await Promise.race(serviciosIA);
+        document.getElementById("resultado").innerText = `${resultado}`;
+      } catch (error) {
+        console.error("Error al generar resumen:", error);
+        res.status(500).json({ message: "Error al generar el resumen." });
+      }
   };
   
-  let texto =
-    "Una aplicación de captura y análisis de protocolos de red (network packet sniffer) es una" +
-    " herramienta que permite obtener y analizar los mensajes intercambiados por aplicaciones" +
-    " de red. Wireshark, la aplicación más utilizada para el análisis de protocolos tiene dos" +
-    " tareas principales: capturar frames de la capa de enlace de datos y mostrar los paquetes de forma tal" +
-    " que se puedan ver los protocolos involucrados con el nivel de detalle deseado.";
 
-  resumir(texto)
-    .then((resultado) => {
-      console.log("El texto resumido es: " + resultado);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  
